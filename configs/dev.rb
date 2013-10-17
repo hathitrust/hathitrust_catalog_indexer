02f5430a1e18e4c92076c4c33e2b4a896df9b26d
Bill Dueber <bill@dueber.com>
2013-10-17 12:04:32 -0400

    Up the resource use

--- configs/dev.rb
+++ configs/dev.rb
@@ -11,7 +11,7 @@ settings do
   provide "solrj_writer.parser_class_name", "XMLResponseParser"
   provide "solrj_writer.commit_on_close", "true"
   provide "solrj_writer.thread_pool", 2
-  provide "solrj_writer.batch_size", 50
+  provide "solrj_writer.batch_size", 200
   provide "writer_class_name", "Traject::SolrJWriter"
-  store 'processing_thread_pool', 3
+  store 'processing_thread_pool', 6
 end
