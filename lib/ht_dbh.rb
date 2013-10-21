9c642f8a711480dc631e531d79369a5bfd3a4122
BillDueber <bill@dueber.com>
2013-10-21 11:59:40 -0400

    Ditch require_relative for dbh

--- lib/ht_dbh.rb
+++ lib/ht_dbh.rb
@@ -1,6 +1,6 @@
 require 'jdbc-helper'
-require_relative 'ht_secure_data'
-require_relative "mysql-connector-java-5.1.17-bin.jar"
+require 'ht_secure_data'
+require "mysql-connector-java-5.1.17-bin.jar"
 
 module HathiTrust
   class DBH
