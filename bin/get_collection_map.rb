d7a65bf2cea65496e18e4b77d88cb45c90c27a38
BillDueber <bill@dueber.com>
2013-10-21 15:14:23 -0400

    Added get_collection utility

---
+++ bin/get_collection_map.rb
@@ -0,0 +1,20 @@
+require 'yaml'
+require 'open-uri'
+
+url = 'http://mirlyn-aleph.lib.umich.edu/hathitrust_collection_map.yaml'
+
+tmap_dir = File.expand_path(File.join("..", 'lib', 'translation_maps', 'ht'), File.dirname(__FILE__))
+
+data = YAML.load(open(url).read)
+
+# Create the "collection_code_to_original_from.yaml" map
+
+output_file = 
+ccof = {}
+data.each_pair do |cc, h|
+  ccof[cc] = h['original_from']
+end
+
+File.open(File.join(tmap_dir, 'collection_code_to_original_from.yaml'), 'w:utf-8') {|f| f.puts ccof.to_yaml}
+
+
