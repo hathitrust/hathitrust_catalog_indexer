e84c3f4776b075028f64384b7262960a33229595
BillDueber <bill@dueber.com>
2013-10-31 13:17:01 -0400

    Move setup code into the common.rb indexer

--- indexers/common.rb
+++ indexers/common.rb
@@ -1,5 +1,32 @@
-#######  COMMON STUFF BETWEEN UMICH AND HT
-#######
+$:.unshift  "#{File.dirname(__FILE__)}/../lib"
+
+require 'traject/macros/marc21_semantics'
+extend  Traject::Macros::Marc21Semantics
+
+require 'traject/macros/marc_format_classifier'
+extend Traject::Macros::MarcFormats
+
+require 'ht_traject'
+extend HathiTrust::Traject::Macros
+extend Traject::UMichFormat::Macros
+
+settings do
+  store "log.batch_progress", 10_000
+end
+
+logger.info RUBY_DESCRIPTION
+
+################################
+###### Setup ###################
+################################
+
+# Set up an area in the clipboard for use storing intermediate stuff
+each_record HathiTrust::Traject::Macros.setup
+
+
+
+#######  COMMON STUFF BETWEEN UMICH AND HT ########
+#######  INDEXING                          ########
 
 
 
