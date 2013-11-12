b07b387b19643abb409266cbcc612733ed405a3f
BillDueber <bill@dueber.com>
2013-11-12 15:04:20 -0500

    Added gzip reader for xml, made it default in readers/xml.rb

---
+++ lib/gzip_xml_reader.rb
@@ -0,0 +1,26 @@
+require 'traject'
+require 'traject/marc4j_reader'
+require 'zlib'
+
+
+
+class Traject::GZipXMLReader < Traject::Marc4JReader
+  def create_marc_reader!
+    case input_type
+    when "binary"
+      permissive = settings["marc4j_reader.permissive"].to_s == "true"
+
+      # #to_inputstream turns our ruby IO into a Java InputStream
+      # third arg means 'convert to UTF-8, yes'
+      MarcPermissiveStreamReader.new(input_stream.to_inputstream, permissive, true, settings["marc4j_reader.source_encoding"])
+    when "xml"
+      if @settings['command_line.filename'] =~ /\.gz$/
+        @input_stream =  Java::java.util.zip.GZIPInputStream.new(@input_stream.to_inputstream)
+      end
+      
+      MarcXmlReader.new(@input_stream)
+    else
+      raise IllegalArgument.new("Unrecgonized marc_source.type: #{input_type}")
+    end
+  end
+end
