80b72b7a5cdaa9c3a59e6e261aa81b327f262fff
Bill Dueber <bill@dueber.com>
2014-05-29 11:56:51 -0400

    Equal vs double-equal error in ht_id_update. Dammit

--- indexers/common_ht.rb
+++ indexers/common_ht.rb
@@ -60,7 +60,7 @@ end
 
 to_field 'ht_id_update' do |record, acc, context|
   acc.concat context.clipboard[:ht][:items].last_update_dates if context.clipboard[:ht][:has_items]
-  acc.delete_if {|x| x = ''}
+  acc.delete_if {|x| x.empty?}
 end
 
 to_field 'ht_json' do |record, acc, context|
