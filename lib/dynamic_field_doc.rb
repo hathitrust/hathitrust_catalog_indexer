06f61b498cae2c556e9332e0e0808b2d6aa62501
Bill Dueber <bill@dueber.com>
2014-11-03 14:36:56 -0500

    Simplifying ddt

--- lib/dynamic_field_doc.rb
+++ lib/dynamic_field_doc.rb
@@ -1,3 +1,5 @@
+$:.unshift '../simple_solr/lib'
+require 'simple_solr'
 module DynamicFieldDocs
 
   TYPES = {
@@ -38,34 +40,50 @@ module DynamicFieldDocs
       '_pp_s' => [:pp, :piped],
   }.to_a.sort{|a,b| b[0].size <=> a[0].size}.map{|x| [Regexp.new("\\A(.+)#{x[0]}\\Z"), x[1]]}.each_with_object({}) {|x,h| h[x[0]] = x[1]}
 
+  def explicit_field_defs
+    surl =    settings['solr.url'] || ENV['SOLR_URL']
+    core = surl.split('/').pop
+    base = surl.split('/')[0..-2].join('/')
+    c = SimpleSolr::Client.new(base, core)
+    c.fields
+  end
 
   def to_dfield(field_name, aLambda=nil, &blk)
 
     outstream = settings["dynamic_field_docs.output_stream"]
 
-    prefix = field_name
-    fielddef = [:stored] # default
 
-    SM.each do |pair|
-      suffix = pair[0]
-      types = pair[1]
-      if match = suffix.match(field_name)
-        prefix = match[1]
-        fielddef = types
-        break
+    @efields ||= explicit_field_defs
+
+    if @efields.has_key?(field_name)
+      f = @efields[field_name]
+      fieldtype = f.type
+      ind = f.indexed ? "indexed" : ""
+      sto = f.stored ? 'stored' : ''
+      outstream.puts '%-25s %-12s %-11s %-11s' % [field_name, fieldtype, ind, sto]
+    else
+      prefix = field_name
+      fielddef = [:stored] # default
+
+      SM.each do |pair|
+        suffix = pair[0]
+        types = pair[1]
+        if match = suffix.match(field_name)
+          prefix = match[1]
+          fielddef = types
+          break
+        end
+      end
+
+      fielddef.each do |fs|
+        t = TYPES[fs]
+        name = prefix + t[0]
+        fieldtype = t[1]
+        ind = t[2] ? "indexed" : ""
+        sto = t[3] ? 'stored' : ''
+        outstream.puts '%-25s %-12s %-11s %-11s' % [name, fieldtype, ind, sto]
       end
     end
-
-    fielddef.each do |fs|
-      t = TYPES[fs]
-      name = prefix + t[0]
-      fieldtype = t[1]
-      ind = t[2] ? "indexed" : "not indexed"
-      sto = t[3] ? 'stored' : 'not stored'
-      outstream.puts '%-25s %-12s %-11s %-11s' % [name, fieldtype, ind, sto]
-    end
-
-
     to_field(field_name, aLambda, &blk)
   end
 
