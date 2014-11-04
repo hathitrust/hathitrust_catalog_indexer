0f5d5a48f213f33cbd4fa874abdfc1eb1e50c137
Bill Dueber <bill@dueber.com>
2014-11-04 14:31:09 -0500

    More complex pop rules

--- indexers/common.rb
+++ indexers/common.rb
@@ -280,6 +280,8 @@ to_field 'place_of_publication' do |r, acc|
   if r['008']
     code = r['008'].value[15..17].gsub(/[^a-z]/, ' ')
 
+    acc << code
+
     # Bail if we've got an explicit "undetermined"
     unless code == 'xx '
       possible_single_letter_country_code = code[2]
@@ -294,17 +296,17 @@ to_field 'place_of_publication' do |r, acc|
 
       # USSR? Check for the two-value version
       if possible_single_letter_country_code == 'r'
+        container = "Soviet Union"
         non_ussr_country = current_map[code[0..1] << ' ']
         if non_ussr_country
-          pop = non_ussr_country
-          container = nil
+          acc <<  non_ussr_country
         end
       end
 
       if pop
         if container
           acc << container
-          acc << "#{container} -- #{pop}"
+          acc << "#{container} -- #{pop}" unless pop == container
         else
           acc << pop
         end
