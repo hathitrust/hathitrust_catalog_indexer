b25652df216077b6d672be388cf2356d92a51bc2
Bill Dueber <bill@dueber.com>
2015-01-27 12:31:22 -0500

    Use stock marc

--- readers/xml.rb
+++ readers/xml.rb
@@ -1,11 +1,7 @@
 $:.unshift  "#{File.dirname(__FILE__)}/../lib"
 require 'traject'
-require 'marc/marc4j'
-require 'traject/marc4j_reader'
-require 'gzip_xml_reader'
 
 
 settings do
-  provide "reader_class_name", "Traject::GZipXMLReader"
   provide "marc_source.type", "xml"
 end
