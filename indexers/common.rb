69df1d697ad8191c195e719cef65fe1ae1677e99
Bill Dueber <bill@dueber.com>
2016-04-25 16:52:26 -0400

    Monkey-patch MARC::FieldMap for speed

--- indexers/common.rb
+++ indexers/common.rb
@@ -20,6 +20,33 @@ settings do
   store "log.batch_progress", 10_000
 end
 
+
+# Monkeypatch MARC::Record for speed
+module MARC
+  class FieldMap
+    def each_by_tag(tags)
+      reindex unless @clean
+      indices = []
+      # Get all the indices associated with the tags
+      Array(tags).each do |t|
+        indices.concat @tags[t] if @tags[t]
+      end
+
+      # Remove any nils
+      indices.compact!
+      return [] if indices.empty?
+
+      # Sort it, so we get the fields back in the order they appear in the record
+      indices.sort!
+
+      indices.each do |tag|
+        yield self[tag]
+      end
+    end
+  end
+end
+
+
 logger.info RUBY_DESCRIPTION
 
 ################################
