bda94fe0ed86bbc430ce8844164bb57cdfc2bf98
Bill Dueber <bill@dueber.com>
2016-06-01 11:04:35 -0400

    Removed any use of jdbc_mysql in favor of sequel

--- bin/get_namespaces.rb
+++ bin/get_namespaces.rb
@@ -1,7 +1,7 @@
-require_relative '../lib/ht_dbh'
+require_relative "../lib/ht_traject/ht_print_holdings"
 
-dbh = HathiTrust::DBH.new
 
-dbh.execute("select namespace, institution from ht_namespaces").each do |row|
-  puts row[0..1].join("\t")
+HathiTrust::PrintHoldings::DB[:ht_namespaces].select(:namespace, :institution).map(&:values).each do |n,i|
+  puts [n,i].join("\t")
 end
+
