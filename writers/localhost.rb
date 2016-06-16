6b27535ba8733ff9d13ffdb9d7584a132f40d71d
Bill Dueber <bill@dueber.com>
2016-06-16 11:28:42 -0400

    Changed to port 8026 and upped thread pool

--- writers/localhost.rb
+++ writers/localhost.rb
@@ -2,9 +2,10 @@ require 'traject'
 require 'traject/solr_json_writer'
 
 settings do
-  provide "solr.url", "http://localhost:8027/solr/biblio"
+  provide "solr.url", "http://localhost:8026/solr/biblio"
   provide "solr_writer.commit_on_close", "true"
   provide "solr_writer.thread_pool", 2
   provide "solr_writer.batch_size", 60
   provide "writer_class_name", "Traject::SolrJsonWriter"
+  provide "processing_thread_pool", 5
 end
