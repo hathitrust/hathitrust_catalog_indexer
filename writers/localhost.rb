0770ce935d78f5091b587939db285f9a58ef691d
billdueber <bill@dueber.com>
2016-06-16 10:11:27 -0400

    Clean up solr config

--- writers/localhost.rb
+++ writers/localhost.rb
@@ -3,8 +3,8 @@ require 'traject/solr_json_writer'
 
 settings do
   provide "solr.url", "http://localhost:8027/solr/biblio"
-  provide "solrj_writer.commit_on_close", "true"
-  provide "solrj_writer.thread_pool", 2
-  provide "solrj_writer.batch_size", 100
+  provide "solr_writer.commit_on_close", "true"
+  provide "solr_writer.thread_pool", 2
+  provide "solr_writer.batch_size", 60
   provide "writer_class_name", "Traject::SolrJsonWriter"
 end
