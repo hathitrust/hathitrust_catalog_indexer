90ca7935ec8333fb33cf090be1d95483fca804a0
Bill Dueber <bill@dueber.com>
2017-06-21 09:01:49 -0400

    Correct for new solr url

--- writers/localhost_ht.rb
+++ writers/localhost_ht.rb
@@ -3,7 +3,7 @@ require 'traject/solr_json_writer'
 
 
 settings do
-  store   "solr.url", "http://localhost:9033/catalog"
+  store   "solr.url", "http://localhost:9033/solr/catalog/"
   provide "solr_writer.commit_on_close", "true"
   provide "solr_writer.thread_pool", 2
   provide "solr_writer.batch_size", 60
