eeacca93f27be456ca10fbcec716b5b4a904987e
moseshll <moseshll@umich.edu>
2023-08-10 17:26:26 -0400

    Appease standardrb

--- spec/cictl/logger_factory_spec.rb
+++ spec/cictl/logger_factory_spec.rb
@@ -17,20 +17,20 @@ RSpec.describe CICTL::LoggerFactory do
   it "sends #error to $stderr" do
     expect {
       testlogger.error "error error-via-error"
-      #testlogger.close
+      # testlogger.close
     }.to output(/error-via-error/).to_stderr_from_any_process
   end
 
   it "sends #fatal to $stderr" do
     expect {
       testlogger.fatal "fatal shwoozle"
-      #testlogger.close
+      # testlogger.close
     }.to output(/shwoozle/).to_stderr_from_any_process
   end
 
   it "sends stuff to the logfile" do
     testlogger.error "error-in-file"
-    #testlogger.close
+    # testlogger.close
     expect(File.read(HathiTrust::Services[:logfile_directory] + "/" + test_log)).to match(/error-in-file/)
   end
 end
