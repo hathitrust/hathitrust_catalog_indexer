f3ea64b9893c8e277d37579e3f0e9e2416bd08ad
moseshll <moseshll@umich.edu>
2023-08-10 15:26:11 -0400

    mkpath

--- lib/cictl/logger_factory.rb
+++ lib/cictl/logger_factory.rb
@@ -22,8 +22,11 @@ module CICTL
       # "Ignoring attempt to add a second console appender: … since it would result in duplicate console output."
       # Lazily apply SemanticLogger config setup here instead of the initializer
       # to make sure no one changes the appenders before the logger is created.
-      SemanticLogger.default_level = min_level
       SemanticLogger.clear_appenders!
+      SemanticLogger.default_level = min_level
+
+      Pathname.new(LogfileDefaults.logdir).mkpath
+
       if @log_file
         SemanticLogger.add_appender(file_name: @log_file, level: min_level)
       end
