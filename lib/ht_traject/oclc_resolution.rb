2d60d59f74d629e031707b989535a8e95bf8cf07
moseshll <moseshll@umich.edu>
2025-02-18 13:16:24 -0500

    DEV-1516 rename oclc concordance fields - Rename fields in `lib/ht_traject/oclc_resolution.rb` Sequel query - Add very basic `OCLCResolution` spec. - Note in passing that `OCLCResolution` is doing a JOIN that on the surface seems terribly inefficient. - This will be ready for a PR when db-image has the updated schema.

--- lib/ht_traject/oclc_resolution.rb
+++ lib/ht_traject/oclc_resolution.rb
@@ -4,8 +4,8 @@ module HathiTrust
   class OCLCResolution
     def self.query
       return @query if @query
-      sql_expr = "select SQL_NO_CACHE o2.oclc, o2.canonical from oclc_concordance o1, oclc_concordance o2
-                  where o2.canonical = o1.canonical and (o1.oclc = ? OR o1.canonical = ?)"
+      sql_expr = "select SQL_NO_CACHE o2.variant, o2.canonical from oclc_concordance o1, oclc_concordance o2
+                  where o2.canonical = o1.canonical and (o1.variant = ? OR o1.canonical = ?)"
 
 
       raw_query = Services[:db][sql_expr, :$oclc, :$oclc]
@@ -15,7 +15,7 @@ module HathiTrust
     def self.all_resolved_oclcs(oclcs)
       oclcs = Array(oclcs).compact
       return [] if oclcs.empty?
-      resolved = oclcs.flat_map{|o| self.query.call(oclc: o)}.flat_map{|x| [x[:oclc], x[:canonical]]}
+      resolved = oclcs.flat_map{|o| self.query.call(oclc: o)}.flat_map{|x| [x[:variant], x[:canonical]]}
       resolved.concat(oclcs).flatten.map(&:to_s).uniq
      end
 
