f2ac311ff59825e65fe38580f0f35ca15900df2c
moseshll <moseshll@umich.edu>
2025-05-15 15:08:14 -0400

    Bump `processing_thread_pool` from 12 to 24.

--- writers/localhost.rb
+++ writers/localhost.rb
@@ -9,6 +9,6 @@ settings do
   provide 'solr_writer.thread_pool', 2
   provide 'solr_writer.batch_size', 60
   provide 'writer_class_name', 'Traject::SolrJsonWriter'
-  provide 'processing_thread_pool', 12
+  provide 'processing_thread_pool', 24
   provide 'log.batch_size', 50_000
 end
