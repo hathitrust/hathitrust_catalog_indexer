bdab8eb58887c37b9893c036d44d7c0d710ccf12
Aaron Elkiss <aelkiss@umich.edu>
2025-05-21 09:30:03 -0400

    Use thread-local storage for holdings api connection
    Although the httpx connection pool should be thread-safe, it seems like
    it isn't scaling very well, and there's no reason for all threads to
    share a pool.

--- lib/ht_traject/print_holdings_api.rb
+++ lib/ht_traject/print_holdings_api.rb
@@ -5,12 +5,14 @@ module HathiTrust
   class PrintHoldingsAPI
 
     def self.connection 
-      @conn ||= Faraday.new( url: ENV["HOLDINGS_API_URL"] ) do |builder|
+      Thread.current[:holdings_api_conn] ||= Faraday.new( url: ENV["HOLDINGS_API_URL"] ) do |builder|
         builder.request :json
         # TODO: Perhaps if it fails we should retry instead of immediately raising an
         # error?
         builder.response :raise_error
         builder.adapter :httpx
+      end.tap do |conn|
+        Services.logger.info("Allocated new connection #{conn} for print holdings api")
       end
     end
 
@@ -30,7 +32,11 @@ module HathiTrust
           ht_json: ht_json
         }
 
-      response = connection.post('/v1/record_held_by', request_body)
+      conn = connection
+
+      Services.logger.debug("Sending api request with #{conn}")
+      response = conn.post('/v1/record_held_by', request_body)
+      Services.logger.debug("Got response for api request with #{conn}")
 
       # map from an array like
       #
